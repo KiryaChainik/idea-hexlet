b059d525034fbb4ed80f8c0a7420c06405022ea0
Кирилл Пантюхов <nefekirik@gmail.com>
2024-08-04 22:29:20 +0300

    New One

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+    implementation("org.apache.commons:commons-lang3:3.15.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
